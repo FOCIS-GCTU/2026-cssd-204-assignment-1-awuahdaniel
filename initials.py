4d6670ec5331d18de242bf2a3aac169a17c479d5
awuahdaniel <awuahdanielagyekum@gmail.com>
2026-05-30 17:13:29 +0000

    Awuah Daniel Assignment-1

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
 
+
 def main():
-  pass #code goes here
+    # File: initials.py
+    # Description: a simple python scrip that prints aout my name initals by a 10x12 dimesion. using only print functions a d strings.
+    # Assigment number: 2
+    #
+    # Name: Awuah Daniel Agyekum
+    # STUDENT ID: 2425402594
+    # Email: awuahdaniel160@gmail.com
+    # 
+    # On my honour AWUAH DANIEL AGYEKUM, this programming assignment is my own 
+    # and i have not provided this code to any other student.
+    print()
+    print("...ADA")
+    print()
+    print('''
+        ........AA.............DDDDDDDD.................AA..........
+        ......AAAAAA...........DDDDDDDDDD.............AAAAAA........
+        .....AAA..AAA..........DD.......DDD..........AAA..AAA.......
+        ....AAA....AAA.........DD........DD.........AAA.....AA......
+        ...AAA......AAA........DD........DD........AAA......AAA.....
+        ...AAAAAAAAAAAA........DD........DD........AAAAAAAAAAAA.....
+        ...AAAAAAAAAAAA........DD........DD........AAAAAAAAAAAA.....
+        ...AAA......AAA........DD.......DDD........AAA......AAA.....
+        ...AAA......AAA...**...DDDDDDDDDD.....**...AAA......AAA...**
+        ...AAA......AAA...**...DDDDDDDD.......**...AAA......AAA...**
+        ''')
+    print()
 
-
-
-main()
